f9a91ed5fbe6c43263915861b15ae2aa4587cc20
tsuri <tsurif@post.bgu.ac.il>
2020-04-09 17:05:56 +0300

    Scalar class update

--- src/Scalar.java
+++ src/Scalar.java
@@ -1,3 +1,11 @@
-public class Scalar extends Object {
-
+public abstract class Scalar extends Object{
+    public boolean isMatch(Scalar s){
+        return this.getType() == s.getType();
+    }
+    public abstract Scalar add(Scalar s);
+    public abstract Scalar mul(Scalar s);
+    public abstract Scalar mul(int i);
+    public abstract Scalar power(int exp);
+    public abstract int sign();
+    protected abstract String getType();
 }
