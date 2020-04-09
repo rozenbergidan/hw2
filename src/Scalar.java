cd6d06a569debc5a09f884a50da01bc77aaa4bb9
tsuri <tsurif@post.bgu.ac.il>
2020-04-09 18:00:24 +0300

    Ratioanl Numbers

--- src/Scalar.java
+++ src/Scalar.java
@@ -1,11 +1,10 @@
 public abstract class Scalar extends Object{
     public boolean isMatch(Scalar s){
-        return this.getType() == s.getType();
+        return ((this instanceof RealScalar & s instanceof RealScalar)|(this instanceof RationalScalar & s instanceof RationalScalar));
     }
     public abstract Scalar add(Scalar s);
     public abstract Scalar mul(Scalar s);
     public abstract Scalar mul(int i);
     public abstract Scalar power(int exp);
     public abstract int sign();
-    protected abstract String getType();
 }
