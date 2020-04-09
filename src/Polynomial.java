2626f82df95286dedd02ec21ff84bf6bb7282556
tsuri <tsurif@post.bgu.ac.il>
2020-04-09 20:27:14 +0300

    biuld

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -1,10 +1,13 @@
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Polynomial {
     private List<Monomial> monomials;
 
-    public Polynomial(){}
+    public Polynomial(){
+        monomials = new LinkedList<>();
+    }
 
     public void build(char type, String input){ // TODO check if the biuld is according to the Instructions
         String[] coefficients = input.split(" ");
@@ -14,17 +17,22 @@ public class Polynomial {
                 if (type == 'Q') {
                     String[] n = coefficient.split("/");
                     int a = Integer.parseInt(n[0]);
-                    int b = 1;
-                    if (n.length > 1) b = Integer.parseInt(n[1]);
-                    RationalScalar c = new RationalScalar(a, b);
-                    monomials.add(new Monomial(c, exp));
+                    if(a != 0) {
+                        int b = 1;
+                        if (n.length > 1) b = Integer.parseInt(n[1]);
+                        RationalScalar c = new RationalScalar(a, b);
+                        monomials.add(new Monomial(c, exp));
+                    }
                 } else {
-                    int v = Integer.parseInt(coefficient);
-                    RealScalar c = new RealScalar(v);
-                    monomials.add(new Monomial(c, exp));
+                    double v = Double.parseDouble(coefficient);
+                    if (v != 0) {
+                        RealScalar c = new RealScalar(v);
+                        monomials.add(new Monomial(c, exp));
+                    }
                 }
                 exp++;
             }
+
         }
     }
     public boolean isMatch(Polynomial p){
@@ -96,17 +104,17 @@ public class Polynomial {
         ans.build(type,str);
         return ans;
     }
-    public Polynomial mul(Polynomial p){
-
-    }
-    public Scalar evaluate(Scalar scalar){
-
-    }
-    public Polynomial derivative(){
-
-    }
-    public String toString(){
-
-    }
+//    public Polynomial mul(Polynomial p){
+//
+//    }
+//    public Scalar evaluate(Scalar scalar){
+//
+//    }
+//    public Polynomial derivative(){
+//
+//    }
+//    public String toString(){
+//
+//    }
 
 }
