356febb205b2b86a8085ba91d67a257987d3a820
rozenbergidan <idanroze@post.bgu.ac.il>
2020-04-09 21:31:17 +0300

    ,,,,

--- src/Scalar.java
+++ src/Scalar.java
@@ -5,7 +5,7 @@ public abstract class Scalar extends Object{
      * learn about:  virtualization, double dispatch and visitor pattern to get 15 points
      */
     public boolean isMatch(Scalar s) {
-        return ((this instanceof RealScalar & s instanceof RealScalar) || (this instanceof RationalScalar & s instanceof RationalScalar));
+        return ((this instanceof RealScalar & s instanceof RealScalar) | (this instanceof RationalScalar & s instanceof RationalScalar));
     }
 
     public abstract Scalar add(Scalar s);
