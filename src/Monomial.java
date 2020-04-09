aa34bac78ce18ac2ea5ef499fabd9012fe43ce8d
rozenbergidan <idanroze@post.bgu.ac.il>
2020-04-09 18:01:45 +0300

    changes

--- src/Monomial.java
+++ src/Monomial.java
@@ -1,2 +1,33 @@
 public class Monomial {
+    private Scalar coe;
+    private int exp;
+
+    public Monomial(Scalar x, int expo){
+        coe=x;
+        exp=expo;
+    }
+    public boolean isMatch(Monomial m){
+
+    }
+    public boolean canAdd(Monomial m){
+
+    }
+    public Monomial add(Monomial m){
+
+    }
+    public Monomial mul(Monomial m){
+
+    }
+    public Scalar evalute(Scalar scalar){
+
+    }
+    public Monomial derivative(){
+
+    }
+    public int sign(){
+
+    }
+    public String toString(){
+
+    }
 }
--- src/Polynomial.java
+++ src/Polynomial.java
@@ -1,2 +1,32 @@
+import java.util.List;
+
 public class Polynomial {
+    private List<Monomial> monomials;
+
+    public Polynomial(){
+        return; //do nothing
+    }
+
+    public void build(char type, String input){
+
+    }
+    public boolean isMatch(Polynomial p){
+
+    }
+    public Polynomial add(Polynomial p){
+
+    }
+    public Polynomial mul(Polynomial p){
+
+    }
+    public Scalar evaluate(Scalar scalar){
+
+    }
+    public Polynomial derivative(){
+
+    }
+    public String toString(){
+
+    }
+
 }
